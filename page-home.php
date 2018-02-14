05f206785090c49ed03b9fdf71d32c85decad6fc
ozpoo <hello@ericandren.com>
2018-02-14 00:39:05 -0800

    P

--- page-home.php
+++ page-home.php
@@ -103,7 +103,13 @@
 
 				video = document.createElement( 'video' );
 				video.loop = true;
-				video.src = "<?php echo get_template_directory_uri(); ?>/assets/video/me.webm";
+
+				if(supportsVideoType('webm') === "probably") {
+				  video.src = "<?php echo get_template_directory_uri(); ?>/assets/video/me.webm";
+				} else {
+				  video.src = "<?php echo get_template_directory_uri(); ?>/assets/video/me.mp4";
+				}
+
 				video.play();
 
 				//Use webcam
@@ -296,6 +302,24 @@
 					scale = 1.45;
 				}
 				plane.scale.x = plane.scale.y = scale;
+			}
+
+			function supportsVideoType(type) {
+			  let video;
+
+			  let formats = {
+			    ogg: 'video/ogg; codecs="theora"',
+			    h264: 'video/mp4; codecs="avc1.42E01E"',
+			    webm: 'video/webm; codecs="vp8, vorbis"',
+			    vp9: 'video/webm; codecs="vp9"',
+			    hls: 'application/x-mpegURL; codecs="avc1.42E01E"'
+			  };
+
+			  if(!video) {
+			    video = document.createElement('video')
+			  }
+
+			  return video.canPlayType(formats[type] || type);
 			}
 
 		});
